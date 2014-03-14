8036d03e37169bac7bb0fa58ebb1eb7a6aba75dd
root <root@alice.octopuce.fr>
2014-03-14 18:03:01 +0100

    init

---
+++ scan.php
@@ -0,0 +1,43 @@
+<?php
+
+set_include_path ( ".:./offwave/lib:".get_include_path());
+if( !is_file(__DIR__."/config.php")){
+
+    die("missing config.php file!");
+}
+
+include("config.php");
+
+
+function __autoload($className) {
+        
+        $ds = DIRECTORY_SEPARATOR;
+	    $className = strtr($className, '_', $ds);
+	        $paths = explode(PATH_SEPARATOR, get_include_path());
+		    foreach($paths as $dir){
+			        $file = "{$dir}{$ds}{$className}.php";
+				        if (is_readable($file)){
+					                require_once $file;
+							            return;
+								            }
+					    }
+		        throw new Exception("Sorry, {$className} is nowhere to be found in
+				".get_include_path());
+};
+
+
+$Scanner = new Offwave_Scanner();
+
+foreach( $pathList as $path ){
+
+	echo "$path\n";
+	$result = $Scanner->scan($path) ;
+	if( isset($result[$path]) && isset ($result[$path]["Spip"])){
+	echo $result[$path]["Spip"]["version"]."\n";
+	}
+
+
+
+}
+
+
