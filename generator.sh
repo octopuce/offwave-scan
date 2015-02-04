e3c7367bd3b7328236b078e6756ef9f7b30ae147
skhaen <skhaen@cyphercat.eu>
2015-02-04 17:11:48 +0100

    [ENH] add the questions, change mindepth and maxdepth

--- generator.sh
+++ generator.sh
@@ -1,10 +1,9 @@
-if [ -z $1 ] ; then
-    echo "[!] Error : missing search path";
-    exit 1;
-fi;
-WD=`realpath $1`;
+read -p "What PATH do you want to scan ? (ex: /var/www/): " WD
+read -p "minimum depth for the scan? (ex: 1) " MinD
+read -p "maximum depth for the scan? (ex: 3) " MaxD
+
 echo $WD;
 echo '<?php $pathList = array(' > config.php;
-/usr/bin/find $WD -not -iname "\.*" -type d -maxdepth 1 -exec echo "\"{}\"," >> config.php \; 
+/usr/bin/find $WD -maxdepth $MinD -maxdepth $MaxD -not -iname "\.*" -type d  -exec echo "\"{}\"," >> config.php \; 
 
 echo ');' >> config.php
